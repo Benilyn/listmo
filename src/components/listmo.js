b287e64e7b11c689c875eaad701dc62ef29b16c0
Benilyn <benilyn.gonzales@gmail.com>
2018-05-03 02:27:35 -0700

    added connect, Route and withRouter and modified code on return to reflect route

--- src/components/listmo.js
+++ src/components/listmo.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Route, withRouter} from 'react-router-dom';
+
+import Header from './header';
 import ProjectList from './project-list';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-
-export default function Listmo() {
-	return (
-      <Router>
-        <div className="listmo">
-            <ProjectList />
-        </div>
-    </Router>
-    );
+export class Listmo extends React.Component {
+	render() {
+		return (
+	        <div className="listmo">
+	        	<Header />
+	            <Route exact path="/" component={ProjectList} />
+	        </div>
+    	);
+	}
+	
 }
+
+export default withRouter(connect()(Listmo))
