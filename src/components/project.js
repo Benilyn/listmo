ac29449fb940c864921d3260ce825729a507005e
Benilyn <benilyn.gonzales@gmail.com>
2018-05-03 10:57:42 -0700

    add link and mapStateProps

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 import Task from './task';
 import AddForm from './add-form';
@@ -8,25 +9,28 @@ import './project.css';
 
 export class Project extends React.Component {
 	addTask(text) {
-		this.props.dispatch(addTask(text, this.props.index));
+		this.props.dispatch(addTask(text, this.props.projectId));
 	}
 
 	render() {
 		const tasks = this.props.tasks.map((task, index) =>
-			<li className="task-name" key={index}>
-				<Task index={index} {...task}/>
+			<li className="task-link" key={index}>
+				<Link to={`/${index}`} {...task}>
+					{task.text} {task.date}
+				</Link>
 			</li>
+
 		);
 		return (
 			<div className="tasks-lists">
 				<h2>{this.props.title}</h2>
 				{tasks}
-				<li className="add-task">
+				<div className="add-task">
 					<AddForm
 						type="task"
 						onAdd={text => this.addTask(text)}
 					/>
-				</li>
+				</div>
 			</div>
 		)
 	}
@@ -38,5 +42,17 @@ Project.defaultProps = {
 	title: ''
 };
 
+const mapStateToProps = (state, props) => {
+	const projectId = props.match.params.projectId;
+	console.log(state);
+	const project = state.projects[projectId];
+	return {
+		projectId,
+		projectName: project.title,
+		tasks: Object.keys(project.tasks).map(taskId =>
+			project.tasks[taskId]
+		)
+	}
+}
 
-export default connect()(Project);
+export default connect(mapStateToProps)(Project);
