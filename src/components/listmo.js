389a58611d7d8656ae014eaec1f5a56a3dd2e1d5
Benilyn <benilyn.gonzales@gmail.com>
2018-05-03 02:07:20 -0700

    modified code to add router

--- src/components/listmo.js
+++ src/components/listmo.js
@@ -1,44 +1,14 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import ProjectList from './project-list';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
-import Project from './project';
-import AddForm from './add-form';
-import {addProject} from '../actions/action-index';
-
-
-
-export class Listmo extends React.Component {
-	addProject(title) {
-		this.props.dispatch(addProject(title));
-	}
-
-	render() {
-		const projects = this.props.projects.map((project, index) =>
-			<ul className="project-name" key={index}>
-				<Project index={index} {...project} />
-			</ul>
-		);
-		return (
-			<div className="listmo">
-				<h3>{this.props.title}</h3>
-				{projects}
-				<li className="add-project">
-					<AddForm
-						type="project"
-						onAdd={title => this.addProject(title)}
-					/>	
-				</li>
-			</div>
-		)
-	}	
+export default function Listmo() {
+	return (
+      <Router>
+        <div className="listmo">
+            <ProjectList />
+        </div>
+    </Router>
+    );
 }
-Listmo.defaultProps = {
-    title: 'Listmo'
-};
-
-const mapStateToProps = state => ({
-    projects: state.projects || []
-});
-
-export default connect(mapStateToProps)(Listmo);
