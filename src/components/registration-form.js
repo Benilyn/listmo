be040607acd93bf41c717a6f159d81243ad76e6c
Benilyn <benilyn.gonzales@gmail.com>
2018-05-10 01:37:45 -0700

    added form fields and onSubmit event

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,14 +1,90 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import {Field, reduxForm, reset} from 'redux-form';
+import {Redirect, withRouter} from 'react-router-dom';
 
+export class RegistrationForm extends React.Component {
+	onSubmit(event) {
+        event.preventDefault();
+        console.log('testing registration-form');
+        return (
+        	<Redirect to="/" />
+        );   	
+    }
 
-export function RegistrationForm() {
-	return (
-		<div className="registration-form">
-			Registration Form
-		</div>
-	)
+	render() {
+		const Button = withRouter(({ history }) => (
+			  <button
+			    type='button'
+			    onClick={() => { history.push('/') }}
+			  >
+			    Submit
+			  </button>
+		))
+		return (
+			<form className="registration-form" onSubmit={e => this.onSubmit(e)}>
+				<h3>{this.props.form}</h3>
+				<div className="firstName">
+					<Field 
+						component="input" 
+						type="text" 
+						name="firstName" 
+						placeholder="First Name" 
+					/>
+				</div>
+				<div className="lasttName">
+					<Field 
+						component="input" 
+						type="text" 
+						name="lasttName" 
+						placeholder="Last Name" 
+					/>
+				</div>
+				<div className="email">
+					<Field 
+						component="input" 
+						type="text" 
+						name="email" 
+						placeholder="Email" 
+					/>
+				</div>
+				<div className="userName">
+					<Field 
+						component="input" 
+						type="text" 
+						name="usertName" 
+						placeholder="Username" 
+					/>
+				</div>
+				<div className="password">
+					<Field 
+						component="input" 
+						type="password" 
+						name="password" 
+						placeholder="Password" 
+					/>
+				</div>
+				<div className="confirmPassword">
+					<Field 
+						component="input" 
+						type="password" 
+						name="confirmPassword" 
+						placeholder="Confirm Password" 
+					/>
+				</div>
+				 <div>
+			        <Button />
+			        <button type="button" onClick={reset}>
+			          Clear Values
+			        </button>
+			      </div>
+				
+			</form>
+		)
+	}
+	
 }
 
 
-export default connect()(RegistrationForm);
+export default reduxForm({
+	form: 'Register User'
+})(RegistrationForm);
