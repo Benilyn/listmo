98680c687bf81532a1c27105bc6a4d70763dd793
Benilyn <benilyn.gonzales@gmail.com>
2018-05-10 11:34:15 -0700

    added formReducer

---
+++ src/reducers/reducers-forms.js
@@ -0,0 +1,23 @@
+import * as actions from '../actions/action-index.js';
+
+const initialState = {
+	user: [{
+		firstName: '',
+		lastName: '',
+		email: '',
+		username: ''
+	}]
+};
+
+export const formReducer = (state=initialState, action) => {
+	if(action.type === actions.ADD_USER) {
+		return Object.assign({}, state, {
+			user: [...state.user, {
+				firstName: action.firstName,
+				lastName: action.lastName,
+				email: action.email,
+				username: action.username
+			}]
+		}); 
+	} {/*actions.ADD_USER*/}
+}
