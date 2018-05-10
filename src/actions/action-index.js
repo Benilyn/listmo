be0def48f6c5ebbb213ddf5d45d487330b29ab14
Benilyn <benilyn.gonzales@gmail.com>
2018-05-10 11:30:56 -0700

    added addUser action

--- src/actions/action-index.js
+++ src/actions/action-index.js
@@ -10,3 +10,9 @@ export const addTask = (text, projectIndex) => ({
 	text,
 	projectIndex
 }); {/*addProject*/}
+
+export const ADD_USER = 'ADD_USER';
+export const addUser = user => ({
+	type: ADD_USER,
+	user
+}); {/*addUser*/}
