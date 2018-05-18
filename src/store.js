1ccae78da130988fa2a18b7268933ba170bd6067
Benilyn <benilyn.gonzales@gmail.com>
2018-05-17 23:49:26 -0700

    added combineReducers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-
+import {reducer as formReducer} from 'redux-form';
 import {listmoReducer} from './reducers/reducers-index.js';
 
-export default createStore(listmoReducer, applyMiddleware(thunk));
-
+export default createStore(
+	combineReducers({
+		form: formReducer,
+		listmoReducer
+	}),
+	applyMiddleware(thunk)
+);
