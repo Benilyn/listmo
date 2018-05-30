c3bc222f0528b23cdd4f9af725cbbe512dc272d6
Benilyn <benilyn.gonzales@gmail.com>
2018-05-30 11:48:10 -0700

    add POST_USER

--- src/actions/action-index.js
+++ src/actions/action-index.js
@@ -1,3 +1,5 @@
+import {API_BASE_URL} from '../config.js';
+
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const addProject = title => ({
 	type: ADD_PROJECT,
@@ -16,3 +18,26 @@ export const addUser = user => ({
 	type: ADD_USER,
 	user
 }); {/*addUser*/}
+
+export const POST_USER = 'POST_USER';
+export const postUser = user => dispatch => {
+	fetch(`${API_BASE_URL}/user`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			firstName: user.firstName,
+        	lastName: user.lastName,
+        	email: user.email,
+        	userName: user.userName,
+        	password: user.password
+		})
+	})
+	.then(res => res.json())
+	.then(success => dispatch(addUser(success)))
+	.catch(err => {
+		console.log(err);
+		return Promise.reject(err);
+	});
+};
