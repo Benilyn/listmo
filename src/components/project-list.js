72811050ecf3abfff218e617372e3977419f48a5
Benilyn <benilyn.gonzales@gmail.com>
2018-06-06 16:59:36 -0700

    add componentDidMount and dispatch getProject

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -1,38 +1,54 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {API_BASE_URL} from '../config.js';
+
 
 import Project from './project';
 import AddProject from './add-project';
-import {addProject} from '../actions/action-index';
+import {addProject, getProject} from '../actions/action-index';
 
 export class ProjectList extends React.Component {
+
+	componentDidMount() {
+		this.props.dispatch(getProject())
+	} //*componentDidMount
+
 	addProject(projectTitle) {
 		this.props.dispatch(addProject(projectTitle));
 	}
 
 	render() {
-		const projects = this.props.projects.map((project, index) =>
-			<li className="project-link" key={index}>
-				<Link to={`/project-list/${index}`}>
-					{project.projectTitle}
-				</Link>
-			</li>
-		); {/*const projects*/}
+		let projectList = this.props.projects.map((project, index) => {
+			return(
+				<li className="project-link" key={index}>
+					<Link to={`/project-list/${index}`}>
+						{project.projectTitle}
+					</Link>
+				</li>
+			) //return
+		}) //*projectList
 
 		return (
 			<div className="project-list">
+				<ul className="project-container">
+					{projectList}
+				</ul>
+				<div className="add-project">
+					<AddProject
+						type="project"
+						onAdd={projectTitle => this.addProject(projectTitle)}
+					/>
+				</div>
+{/*
 				<ul className="project-list">
 					<h3>{this.props.title}</h3>
 					{projects}
-
-					<div className="add-project">
-						<AddProject
-							type="project"
-							onAdd={projectTitle => this.addProject(projectTitle)}
-						/>
-					</div>
 				</ul>
+*/}
+
+
+
 			</div>
 		); {/*return*/}
 	}
