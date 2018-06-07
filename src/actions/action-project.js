5197868366245a1f8808c2726e5bc3a20f752534
Benilyn <benilyn.gonzales@gmail.com>
2018-06-06 17:19:05 -0700

    separated actions and update other elements to reflect new action elements

--- src/actions/action-project.js
+++ src/actions/action-project.js
@@ -6,13 +6,6 @@ export const addProject = project => ({
 	project
 }); {/*addProject*/}
 
-export const ADD_TASK = 'ADD_TASK';
-export const addTask = (projectTask, projectIndex) => ({
-	type: ADD_TASK,
-	projectTask,
-	projectIndex
-}); {/*addProject*/}
-
 export const postProject = project => dispatch => {
 	fetch(`${API_BASE_URL}/project`, {
 		method: 'POST',
@@ -55,25 +48,3 @@ export const getProjectSuccess = projects => ({
 	type: GET_PROJECT_SUCCESS,
 	projects
 }); {/*addProject*/}
-
-
-export const postTask = task => dispatch => {
-	fetch(`${API_BASE_URL}/task`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			taskTitle: task.taskTitle,
-			taskDueDate: task.taskDueDate,
-			taskDetail: task.taskDetail,
-			taskProject: task.taskProject
-		})
-	})
-	.then(res => res.json())
-	.then(success => dispatch(addProject(success)))
-	.catch(err => {
-		console.log(err);
-		return Promise.reject(err);
-	}); //fetch(`${API_BASE_URL}/task`
-}; //const postTask
