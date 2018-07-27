f6ad76ddfb8602a5f3b1ae4958570546a6b1338d
Benilyn <benilyn.gonzales@gmail.com>
2018-07-27 10:57:57 -0700

    add smoke test

---
+++ src/components/listmo.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import Listmo from './listmo';
+
+describe('<Listmo />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Listmo />);
+    });
+});
