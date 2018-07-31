489ba2e4e6c282bcf72b3081162c09f5b6fdd6e5
Benilyn <benilyn.gonzales@gmail.com>
2018-07-31 10:57:45 -0700

    added smoke test

--- src/components/register-user.test.js
+++ src/components/register-user.test.js
@@ -3,7 +3,24 @@ import {shallow, mount} from 'enzyme';
 import RegisterUser from './register-user';
 
 describe('<RegisterUser />', () => {
+    const seedUser = {
+        userName: 'username1',
+        password: 'password'
+    };
+
     it('Renders without crashing', () => {
         shallow(<RegisterUser />);
+    });
+
+    it('Should dispatch postUser when form is submitted', () => {
+      const dispatch = jest.fn();
+      const wrapper = shallow(<RegisterUser dispatch={dispatch} />);
+      const seedUser = {
+          userName: 'username1',
+          password: 'password'
+      };
+      wrapper.instance().onSubmit(seedUser);
+      wrapper.simulate('submit');
+      expect(dispatch).toHaveBeenCalledWith(postUser(seedUser));
     });
 });
