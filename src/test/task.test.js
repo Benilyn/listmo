66b947ddb21bd080935b1a8ef7c1362f2ce2f178
Benilyn <benilyn.gonzales@gmail.com>
2019-07-15 08:44:50 -0700

    removed test

--- src/test/task.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
-import {Task} from '../components/task';
-
-describe('<Task />', () => {
-    it('Renders without crashing', () => {
-        shallow(<Task />);
-    });
-});
